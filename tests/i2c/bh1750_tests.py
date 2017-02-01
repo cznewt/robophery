68baba49ce06420f7b2cc93586d123ead9aa7e88
Martin Vacula <vacula@gmail.com>
2017-02-01 18:26:23 +0100

    test update

--- tests/i2c/bh1750_tests.py
+++ tests/i2c/bh1750_tests.py
@@ -1,14 +1,17 @@
-from robophery.i2c import Bh1750Module
+import sys
+import time
+sys.path.append('/root/sdcard/git/robophery-dev/')
+
+from robophery.i2c.bh1750 import Bh1750Module
 
 def main():
     cfg = {'name': 'hydro-light', 'bus': 2}
-
-    Bh1750Module sensor(cfg);
-
+    
+    bh = Bh1750Module(cfg);
+    
     while True:
-        print('while loop')
+        bh.get_data()
         time.sleep(1)
-
 
 if __name__=="__main__":
     main()
