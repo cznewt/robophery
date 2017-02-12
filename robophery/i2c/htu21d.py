405fc9b61f949d4bdbff6d84463752347f9a6103
Ales Komarek <ales.komarek@newt.cz>
2017-02-12 17:26:42 +0100

    I2C fixes

--- robophery/i2c/htu21d.py
+++ robophery/i2c/htu21d.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import math
 from robophery.i2c import I2cModule
 
 
-class Htu21dException(Exception):
-    pass
-
-
 class Htu21dModule(I2cModule):
-
+    """
+    Module for HTU21D temperature and humidity sensor.
+    """
+    DEVICE_NAME = 'i2c-htu21d'
     # HTU21D default address
-    HTU21D_I2C_ADDR = 0x40
+    DEVICE_ADDR = 0x40
     # Operating modes
     HTU21D_HOLD_MASTER = 0x00
     HTU21D_NOHOLD_MASTER = 0x10
@@ -27,10 +23,9 @@ class Htu21dModule(I2cModule):
     HTU21D_B = 1762.39
     HTU21D_C = 235.66
 
-    def __init__(self, **kwargs):
-
-        super(Htu21dModule, self, **kwargs).__init__()
-
+    def __init__(self, *args, **kwargs):
+        self._addr = self.DEVICE_ADDR
+        super(Htu21dModule, self).__init__(*args, **kwargs)
         # Check that mode is valid.
         if mode not in [self.HTU21D_HOLD_MASTER, self.HTU21D_NOHOLD_MASTER]:
             raise ValueError('Unexpected mode value {0}.'.format(mode))
@@ -58,10 +53,10 @@ class Htu21dModule(I2cModule):
         """
         msb, lsb, chsum = self.readList(self.HTU21D_TRIGGERTEMPCMD, 3)
         if self.crc_check(msb, lsb, chsum) is False:
-            raise Htu21dException("CRC Exception")
+            raise RuntimeError("CRC Exception")
         raw = (msb << 8) + lsb
         raw &= 0xFFFC
-        self._logger.debug('Raw temp 0x{0:X} ({1})'.format(raw & 0xFFFF, raw))
+        self._log.debug('Raw temp 0x{0:X} ({1})'.format(raw & 0xFFFF, raw))
         return raw
 
 
@@ -72,10 +67,10 @@ class Htu21dModule(I2cModule):
         """
         msb, lsb, chsum = self.readList(self.HTU21D_TRIGGERHUMIDITYCMD, 3)
         if self.crc_check(msb, lsb, chsum) is False:
-            raise Htu21dException("CRC Exception")
+            raise RuntimeError("CRC Exception")
         raw = (msb << 8) + lsb
         raw &= 0xFFFC
-        self._logger.debug('Raw relative humidity 0x{0:04X} ({1})'.format(raw & 0xFFFF, raw))
+        self._log.debug('Raw relative humidity 0x{0:04X} ({1})'.format(raw & 0xFFFF, raw))
         return raw
 
 
@@ -128,12 +123,11 @@ class Htu21dModule(I2cModule):
         """
         Get all sensor readings.
         """
-        values = [
-            ('%s.temperature' % self.name, self.get_temperature, ),
-            ('%s.humidity' % self.name, self.get_humidity, ),
-            ('%s.dew_point_temperature' % self.name, self.get_dew_point, ),
+        return [
+            (self._name, 'temperature', self.get_temperature, ),
+            (self._name, 'humidity', self.get_humidity, ),
+            (self._name, 'dew_point_temperature', self.get_dew_point, ),
         ]
-        return values
 
 
     @property
