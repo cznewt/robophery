6d621e1047c6c4e706cfd22cfa551b899f58046d
Ales Komarek <ales.komarek@newt.cz>
2017-03-12 10:58:25 +0100

    Comm tests

--- robophery/comm/linux/mqtt.py
+++ robophery/comm/linux/mqtt.py
@@ -8,7 +8,7 @@ class PahoMqttComm(MqttComm):
         #self._client.on_connect = self._on_connect
         #self._client.on_message = self._on_message
         self._host = kwargs.get('host', '172.0.0.1')
-        self._port = kwargs.get('host', 1883)
+        self._port = kwargs.get('port', 1883)
         self._client.connect(self._host, self._port, 60)
         #self._client.loop_forever()
         super(PahoMqttComm, self).__init__(*args, **kwargs)
