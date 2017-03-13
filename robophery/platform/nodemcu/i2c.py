08e7a275bf7fba8b4c649bb4b9c6d2ff2ecfaf4b
Martin Vacula <vacula@gmail.com>
2017-03-13 15:42:31 +0100

    nodemcu i2c interface

--- robophery/platform/nodemcu/i2c.py
+++ robophery/platform/nodemcu/i2c.py
@@ -1,81 +1,77 @@
+from ustruct import (pack, unpack)
 
-from pyb import I2C
-from robophery.platform.i2c import I2cInterface
+class NodemcuI2cInterface():
+    def __init__(self, scl_pin=5, sda_pin=4, frequency=100000):
+        from machine import Pin, I2C
 
-class NodeMcuI2cInterface(I2cInterface):
+        self._bus = I2C(scl=Pin(scl_pin), sda=Pin(sda_pin), freq=frequency)
 
-    def __init__(self, address):
-        self._bus = I2C(port, I2C.MASTER)
-        super(NodeMcuI2cInterface, self).__init__(*args, **kwargs)
-
+    def scan_bus():
+        """Return list of found addresses on bus"""
+        return self._bus.scan()
 
     def writeRaw8(self, addr, value):
         """Write an 8-bit value on the bus (without register)."""
         value = value & 0xFF
-        self._bus.send(value, addr)
-
+        self._bus.writeto(addr, bytes([value]))
 
     def write8(self, addr, register, value):
         """Write an 8-bit value to the specified register."""
         value = value & 0xFF
-        self._bus.mem_write(value, addr, register)
-
+        self._bus.writeto_mem(addr, register, bytes([value]))
 
     def write16(self, addr, register, value):
         """Write a 16-bit value to the specified register."""
-        value = value & 0xFFFF
-        self._bus.mem_write(value, addr, register)
-
+        bvalue = pack('>H', (value & 0xFFFF))
+        self._bus.writeto_mem(addr, register, bvalue)
 
     def writeList(self, addr, register, data):
         """Write bytes to the specified register."""
-        self._bus.mem_write(data, addr, register)
+        bvalue = bytes(data)
+        self._bus.writeto_mem(addr, register, bvalue)
 
-
-    def readRaw8(self):
+    def readRaw8(self, addr):
         """Read an 8-bit value on the bus (without register)."""
-        result = self._bus.recv(1, addr)
+        bvalue = self._bus.readfrom(addr, 1)
+        result = unpack('B', bvalue)[0]
         return result
-
 
     def readU8(self, addr, register):
         """Read an unsigned byte from the specified register."""
-        result = self._bus.mem_read(1, addr, register)
+        bvalue = self._bus.readfrom_mem(addr, register, 1)
+        result = unpack('B', bvalue)[0]
         return result
-
 
     def readS8(self, addr, register):
         """Read a signed byte from the specified register."""
-        result = readU8(register)
-        if result > 127:
-            result -= 256
+        bvalue = self._bus.readfrom_mem(addr, register, 1)
+        result = unpack('b', bvalue)[0]
         return result
-
 
     def readU16(self, addr, register, little_endian=True):
         """Read an unsigned 16-bit value from the specified register, with the
         specified endianness (default little endian, or least significant byte
         first)."""
-        result = self._bus.mem_read(2, addr, register)
-        if not little_endian:
-            result = ((result << 8) & 0xFF00) + (result >> 8)
+        bvalue = self._bus.readfrom_mem(addr, register, 2)
+        if little_endian is True:
+            result = unpack('<H', bvalue)[0]
+        else:
+            result = unpack('>H', bvalue)[0]
         return result
-
 
     def readS16(self, addr, register, little_endian=True):
         """Read a signed 16-bit value from the specified register, with the
         specified endianness (default little endian, or least significant byte
         first)."""
-        result = self.readU16(register, little_endian)
-        if result > 32767:
-            result -= 65536
+        bvalue = self._bus.readfrom_mem(addr, register, 2)
+        if little_endian is True:
+            result = unpack('<h', bvalue)[0]
+        else:
+            result = unpack('>h', bvalue)[0]
         return result
-
 
     def readList(self, addr, register, length):
         """Read a length number of bytes from the specified register.  Results
         will be returned as a bytearray."""
-        #TODO
-        #result = self._bus.mem_read(buffer, addr, register)
-        #return results
-        raise NotImplementedError()
+        bvalue = self._bus.readfrom_mem(addr, register, length)
+        return list(bvalue)
