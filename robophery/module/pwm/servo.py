c7794dc18c67c61bcb8c112587f707b22feb42cb
Ales Komarek <ales.komarek@newt.cz>
2017-03-19 19:25:49 +0100

    PWM servo

--- robophery/module/pwm/servo.py
+++ robophery/module/pwm/servo.py
@@ -12,4 +12,4 @@ class ServoModule(PwmModule):
     def __init__(self, *args, **kwargs):
         self._pin = self._normalize_pin(kwargs.get('data_pin'))
         super(ServoModule, self).__init__(*args, **kwargs)
-        self.setup_pin(self._pin)
+        self.setup_pin(self._pin, 0)
