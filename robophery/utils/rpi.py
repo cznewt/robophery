b8c7f56e6fa4e7c7bf297c12b3593d8d9d7aba96
Michael Kutý <6du1ro.n@gmail.com>
2017-04-09 11:39:40 +0200

    Add initial tests environment.

--- robophery/utils/rpi.py
+++ robophery/utils/rpi.py
@@ -1,5 +1,6 @@
 import re
 
+
 def detect_pi_version():
     """
     Detect the version of the Raspberry Pi.  Returns either 1, 2 or
@@ -11,23 +12,26 @@ def detect_pi_version():
     * 2709 is pi 2
     * Anything else is not a pi.
     """
-    with open('/proc/cpuinfo', 'r') as infile:
-        cpuinfo = infile.read()
-    # Match a line like 'Hardware   : BCM2709'
-    match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo,
-                      flags=re.MULTILINE | re.IGNORECASE)
-    if not match:
-        # Couldn't find the hardware, assume it isn't a pi.
-        return None
-    if match.group(1) == 'BCM2708':
-        # Pi 1
-        return 1
-    elif match.group(1) == 'BCM2709':
-        # Pi 2
-        return 2
-    else:
-        # Something else, not a pi.
-        return None
+
+    try:
+        with open('/proc/cpuinfo', 'r') as infile:
+            cpuinfo = infile.read()
+        # Match a line like 'Hardware   : BCM2709'
+        match = re.search('^Hardware\s+:\s+(\w+)$', cpuinfo,
+                          flags=re.MULTILINE | re.IGNORECASE)
+        if not match:
+            # Couldn't find the hardware, assume it isn't a pi.
+            return None
+        if match.group(1) == 'BCM2708':
+            # Pi 1
+            return 1
+        elif match.group(1) == 'BCM2709':
+            # Pi 2
+            return 2
+    except IOError:
+        pass
+
+    return None
 
 
 def detect_pi_revision():
@@ -41,8 +45,10 @@ def detect_pi_revision():
     with open('/proc/cpuinfo', 'r') as infile:
         for line in infile:
             # Match a line of the form "Revision : 0002" while ignoring extra
-            # info in front of the revsion (like 1000 when the Pi was over-volted).
-            match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
+            # info in front of the revsion (like 1000 when the Pi was
+            # over-volted).
+            match = re.match(
+                'Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
             if match and match.group(1) in ['0000', '0002', '0003']:
                 # Return revision 1 if revision ends with 0000, 0002 or 0003.
                 return 1
