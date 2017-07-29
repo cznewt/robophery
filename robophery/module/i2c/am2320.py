60bfc5b97ed381244a3df8d11d86b771c2c96f0a
Ales Komarek <ales.komarek@newt.cz>
2017-07-29 02:26:39 +0200

    Fixed AM2320 readings

--- robophery/module/i2c/am2320.py
+++ robophery/module/i2c/am2320.py
@@ -1,10 +1,10 @@
 from struct import unpack
 from robophery.interface.i2c import I2cModule
 
-I2C_ADDR_AM2320 = 0x5c  # 0xB8 >> 1
 AM2320_READ_PARAM = 0x03
 AM2320_HUMIDITY_MSB = 0x00
 AM2320_HUMIDITY_LSB = 0x01
+AM2320_TEMPERATURE_MNB = 0x04
 AM2320_TEMPERATURE_MSB = 0x02
 AM2320_TEMPERATURE_LSB = 0x03
 AM2320_DEVICE_ID_BIT_24_31 = 0x0B
@@ -30,36 +30,56 @@ class Am2320Module(I2cModule):
             self._data.writeList(0x00, [])
             self._data.writeList(command, [regaddr, regcount])
 
-            self._msleep(2)
+            self._msleep(1.6)
 
-            buf = self._data.readList(0, 8)
+            data = self._data.readList(0, 8)
         except IOError, exc:
             raise CommunicationError(str(exc))
 
-        buf_str = "".join(chr(x) for x in buf)
+        # Check data[0] and data[1]
+        if data[0] != 0x03 or data[1] != 0x04:
+            raise Exception("First two read bytes are a mismatch")
 
-        crc = unpack('<H', buf_str[-2:])[0]
-        if crc != self._am_crc16(buf[:-2]):
-            raise CommunicationError("AM2320 CRC error.")
-        return buf_str[2:-2]
+        # CRC check
+        if self._calc_crc16(data[0:6]) != self._combine_bytes(data[7], data[6]):
+            raise Exception("CRC failed")
 
-    def _am_crc16(self, buf):
+        # Temperature resolution is 16Bit, temperature highest bit (Bit15) is
+        # equal to 1 indicates a negative temperature, the temperature highest
+        # bit (Bit15) is equal to 0 indicates a positive temperature;
+        # temperature in addition to the most significant bit (Bit14 ~ Bit0)
+        # indicates the temperature sensor string value. Temperature sensor
+        # value is a string of 10 times the actual temperature value.
+
+        temp = self._combine_bytes(data[4], data[5])
+        if temp & 0x8000:
+          temp = -(temp & 0x7FFF)
+        temp /= 10.0
+
+        humi = self._combine_bytes(data[2], data[3]) / 10.0
+
+        return (temp, humi)
+
+    def _calc_crc16(data):
         crc = 0xFFFF
-        for c in buf:
-            crc ^= c
-            for i in range(8):
-                if crc & 0x01:
+        for x in data:
+            crc = crc ^ x
+            for bit in range(0, 8):
+                if (crc & 0x0001) == 0x0001:
                     crc >>= 1
                     crc ^= 0xA001
                 else:
                     crc >>= 1
         return crc
 
+    def _combine_bytes(msb, lsb):
+        return msb << 8 | lsb
+
     def read_uid(self):
         """
         Read and return unique 32bit sensor ID.
         """
-        resp = self._read_raw(AM2320_READ_PARAM, AM2320_DEVICE_ID_BIT_24_31, 4)
+        resp = self._read_raw(AM2320_READ_PARAM, AM2320_DEVICE_ID_BIT_24_31, AM2320_TEMPERATURE_MNB)
         uid = unpack('>I', resp)[0]
         return uid
 
@@ -68,9 +88,7 @@ class Am2320Module(I2cModule):
         Query AM2320 to get the humidity and temperature readings.
         """
         read_start = self._get_time()
-        raw_data = self._read_raw(AM2320_READ_PARAM, AM2320_HUMIDITY_MSB, 4)
-        temperature = unpack('>H', raw_data[-2:])[0] / 10.0
-        humidity = unpack('>H', raw_data[-4:2])[0] / 10.0
+        temperature, humidity = self._read_raw(AM2320_READ_PARAM, AM2320_HUMIDITY_MSB, AM2320_TEMPERATURE_MNB)
         read_time = (self._get_time() - read_start) / 2
         data = [
             (self._name, 'temperature', temperature, read_time),
