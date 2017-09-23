816eb02720afaa2aadc9ee97d978f40780594cc8
Ales Komarek <ales.komarek@newt.cz>
2017-09-23 13:02:15 +0200

    Ezo EC module ready

--- robophery/module/i2c/ezoec.py
+++ robophery/module/i2c/ezoec.py
@@ -8,54 +8,80 @@ class EzoEcModule(I2cModule):
     DEVICE_NAME = 'ezoec'
     DEVICE_ADDR = 100
     SLAVE_ADDR = 0x703
-
+    CMD_READ = 0x52
+    CMD_STOP = 0x00
     # the timeout needed to query readings and calibrations
-    LONG_TIMEOUT = 1.5
+    TIMEOUT_LONG = 700
     # timeout for regular commands
-    SHORT_TIMEOUT = 0.5
+    TIMEOUT_SHORT = 400
 
     def __init__(self, *args, **kwargs):
         super(EzoEcModule, self).__init__(*args, **kwargs)
         self._data = self._setup_i2c_iface(kwargs.get('data'))
 
-    def write(self, cmd):
-        # appends the null character and sends the string over I2C
-        cmd += "\00"
-        self.file_write.write(cmd)
-
-    def read(self, num_of_bytes=31):
-        """
-        Reads a specified number of bytes from I2C, then parses and displays
-        the result.
-        """
-        res = self.file_read.read(num_of_bytes)  # read from the board
-        # remove the null characters to get the response
-        response = filter(lambda x: x != '\x00', res)
-        if ord(response[0]) == 1:  # if the response isn't an error
-            # change MSB to 0 for all received characters except the first and
-            # get a list of characters
-            char_list = map(lambda x: chr(ord(x) & ~0x80), list(response[1:]))
-            # NOTE: having to change the MSB to 0 is a glitch in the raspberry
-            # pi, and you shouldn't have to do this!
-            # convert the char list to a string and returns it
-            return "Command succeeded " + ''.join(char_list)
-        else:
-            return "Error " + str(ord(response[0]))
-
-    def query(self, string):
+    def _read_data(self):
         """
         Write a command to the board, wait the correct timeout, and read
         the response.
         """
-        self.write(string)
-
-        # the read and calibration commands require a longer timeout
-        if((string.upper().startswith("R")) or
-                (string.upper().startswith("CAL"))):
-            self._sleep(self.long_timeout)
-        elif string.upper().startswith("SLEEP"):
-            return "sleep mode"
+        self._data.writeRaw8(self.CMD_READ)
+        self._data.writeRaw8(self.CMD_STOP)
+        self._msleep(self.TIMEOUT_LONG)
+        response_raw = self._data.readList(0, 31)
+        if response_raw[0] == 1:
+            response = filter(lambda x: x != 0, response_raw)
+            char_list = map(lambda x: chr(x), list(response[1:]))
+            vals = "".join(char_list).split(',')
+            return float(vals[0]), float(vals[1]), float(vals[2]), float(vals[3])
         else:
-            self._sleep(self.short_timeout)
+            return None
 
-        return self.read()
+    def read_data(self):
+        """
+        Get all sensor readings.
+        """
+        read_start = self._get_time()
+        try:
+            ec, tds, s, sg = self._read_data()
+        except IOError:
+            ec, tds, s, sg = None
+        read_stop = self._get_time()
+        read_time = (read_stop - read_start) / 4
+        data = [
+            (self._name, 'conductivity', ec, read_time),
+            (self._name, 'total_dissolved_solids', tds, read_time),
+            (self._name, 'salinity', s, read_time),
+        ]
+        self._log_data(data)
+        return data
+
+    def meta_data(self):
+        """
+        Get the readings meta-data.
+        """
+        return {
+            'conductivity': {
+                'type': 'gauge',
+                'unit': 'uS/cm',
+                'precision': 100.00,
+                'range_low': 0.07,
+                'range_high': 500000,
+                'sensor': self.DEVICE_NAME
+            },
+            'total_dissolved_solids': {
+                'type': 'gauge',
+                'unit': 'ppm',
+                'precision': 1000,
+                'range_low': 0,
+                'range_high': 1000000,
+                'sensor': self.DEVICE_NAME
+            },
+            'salinity': {
+                'type': 'gauge',
+                'unit': 'ppt',
+                'precision': 1,
+                'range_low': 0,
+                'range_high': 1000,
+                'sensor': self.DEVICE_NAME
+            },
+        }
--- robophery/module/i2c/ezoph.py
+++ robophery/module/i2c/ezoph.py
@@ -8,49 +8,24 @@ class EzoPhModule(I2cModule):
     DEVICE_NAME = 'ezoph'
     DEVICE_ADDR = 99
     SLAVE_ADDR = 0x703
-
+    CMD_READ = 0x52
+    CMD_STOP = 0x00
     # the timeout needed to query readings and calibrations
-    TIMEOUT_LONG = 1500
+    TIMEOUT_LONG = 1000
     # timeout for regular commands
-    TIMEOUT_SHORT = 500
+    TIMEOUT_SHORT = 400
 
     def __init__(self, *args, **kwargs):
         super(EzoPhModule, self).__init__(*args, **kwargs)
         self._data = self._setup_i2c_iface(kwargs.get('data'))
-
-    def _write_cmd(self, cmd):
-        """
-        Appends the null character and sends the string over I2C
-        """
-        cmd += 0x00
-        self.file_write.write(cmd)
-
-    def _cmd_read(self, num_of_bytes=31):
-        """
-        Reads a specified number of bytes from I2C, then parses and displays
-        the result.
-        """
-        res = self.file_read.read(num_of_bytes)  # read from the board
-        # remove the null characters to get the response
-        response = filter(lambda x: x != '\x00', res)
-        if ord(response[0]) == 1:  # if the response isn't an error
-            # change MSB to 0 for all received characters except the first and
-            # get a list of characters
-            char_list = map(lambda x: chr(ord(x) & ~0x80), list(response[1:]))
-            # NOTE: having to change the MSB to 0 is a glitch in the raspberry
-            # pi, and you shouldn't have to do this!
-            # convert the char list to a string and returns it
-            return "Command succeeded " + ''.join(char_list)
-        else:
-            return "Error " + str(ord(response[0]))
 
     def _read_data(self):
         """
         Write a command to the board, wait the correct timeout, and read
         the response.
         """
-        self._data.writeRaw8(0x52)
-        self._data.writeRaw8(0x00)
+        self._data.writeRaw8(self.CMD_READ)
+        self._data.writeRaw8(self.CMD_STOP)
         self._msleep(self.TIMEOUT_LONG)
         response_raw = self._data.readList(0, 31)
         if response_raw[0] == 1:
@@ -69,7 +44,6 @@ class EzoPhModule(I2cModule):
             ph = self._read_data()
         except IOError:
             ph = None
-        print ph
         read_stop = self._get_time()
         read_time = read_stop - read_start
         data = [
